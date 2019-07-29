b6b69a9fc7848f5e5d2117fe0993206b36d50cdf
aidevnn <ilyas.kadassi@gmail.com>
2019-07-29 17:19:41 +0300

    FirstCommit

---
+++ DesertLand/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesertLand
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
